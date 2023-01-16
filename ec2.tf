9b47f6924b1f834cd5c07b8616e960069d5e565e
ALagierski <aleksanderlagierski@gmail.com>
2023-01-16 18:26:15 +0000

    ec2.tf fixes

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ data "aws_ami" "latest_ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvmssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 }
 
@@ -13,7 +13,7 @@ resource "aws_instance" "panda" {
     null_resource.download_ssh_key
   ]
   count                  = length(var.availability_zones)
-  ami                    = data.aws_ami.latest_ubuntu
+  ami                    = data.aws_ami.latest_ubuntu.image_id
   instance_type          = "t2.micro"
   availability_zone      = var.availability_zones[count.index]
   key_name               = var.aws_key_name
