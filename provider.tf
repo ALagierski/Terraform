4187560071f629338e2ee15a57831ee8d797af8b
ALagierski <aleksanderlagierski@gmail.com>
2023-01-16 16:48:57 +0000

    provider.tf

---
+++ provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "olek-lagierski-panda-devops-11"
+    key    = "infra/terraform.state"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.33.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
